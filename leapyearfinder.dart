f3bca3caf9fc05f4d3022daac5f0ac971284fde1
Awais Khan <theawaiskhan97@gmail.com>
2026-06-14 22:48:12 +0500

    Update leapyearfinder.dart

--- leapyearfinder.dart
+++ leapyearfinder.dart
@@ -1,6 +1,9 @@
-void main() {
-  int year = 2024;
+import 'dart:io';
 
+void main() {
+  int year;
+  stdout.write("Enter a year: ");
+  year = int.parse(stdin.readLineSync()!);
   if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
     print("$year is a Leap Year ✅");
   } else {
